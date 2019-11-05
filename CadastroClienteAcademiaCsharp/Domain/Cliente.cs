5125582a35f996df897f085d0e96dde1b4e7ffcd
Carlos Cesar Paza <carlos.cesar@hiper.com.br>
2019-11-05 17:08:54 -0300

    Ajuste de Guid

--- CadastroClienteAcademiaCsharp/Domain/Cliente.cs
+++ CadastroClienteAcademiaCsharp/Domain/Cliente.cs
@@ -7,7 +7,7 @@ namespace CadastroClienteAcademiaCsharp.Domain
         public Guid Id { get; set; }
         public int Codigo { get; set; }
         public string Nome { get; set; }
-        public Guid? CidadeId { get; set; }
+        public Guid CidadeId { get; set; }
         public Cidade Cidade { get; set; }
         public string Telefone { get; set; }
         public DateTime DataDeCadastro { get; set; }
